cfdfd84eec95c9cdd78df9690a821f48031d4c03
Alberto Restifo <alberto.restifo@gmail.com>
2017-05-21 10:59:52 +0200

    Change font for Fira Code

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Roboto Mono for Powerline:pixelsize=11";
+static char font[] = "Fira Mono for Powerline:pixelsize=11";
 static int borderpx = 10;
 
 /*
